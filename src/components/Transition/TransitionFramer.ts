fcbd6202021a1f8d3c730fd430f03cfed4657ce9
Harold Steven Cabrera González <harold665.hscg@gmail.com>
2025-07-11 14:32:00 -0600

    fixing an error

--- src/components/Transition/TransitionFramer.ts
+++ src/components/Transition/TransitionFramer.ts
@@ -1,26 +1,26 @@
-export const fadeIn = () => {
-  return {
-    hidden: {
-      y: 100,
-      opacity: 0,
-      x: 0,
-      transition: {
-        type: "tween",
-        duration: 0.8,
-        delay: 0.8,
-        ease: [0.25, 0.1, 0.25, 1], // ✔️ Válido array de 4 valores
-      },
+import { Variants } from "framer-motion";
+
+export const fadeIn = (): Variants => ({
+  hidden: {
+    y: 100,
+    opacity: 0,
+    x: 0,
+    transition: {
+      type: "tween" as const,
+      duration: 0.8,
+      delay: 0.8,
+      ease: [0.25, 0.1, 0.25, 1],
     },
-    visible: {
-      y: 0,
-      x: 0,
-      opacity: 1,
-      transition: {
-        type: "tween",
-        duration: 0.7,
-        delay: 0.7,
-        ease: [0.25, 0.25, 0.25, 0.75], 
-      },
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "tween" as const,
+      duration: 0.7,
+      delay: 0.7,
+      ease: [0.25, 0.25, 0.25, 0.75],
     },
-  };
-};
+  },
+});
